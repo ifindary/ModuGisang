93f18ebaade26d8bc214ecdf24758e16890c1757
pongpongie <arevolvera@gmail.com>
2024-12-05 20:42:20 +0900

    feat: add ChallengeEditBtn to Setting page

--- frontend/src/pages/Settings/Settings.js
+++ frontend/src/pages/Settings/Settings.js
@@ -33,6 +33,7 @@ const Settings = () => {
   const [isAbleInput, setIsAbleInput] = useState(false);
   const [isExceeded30, setIsExceeded30] = useState(false);
 
+  const [isHost, setIsHost] = useState(false);
   //////////////////////////////////////////
   // const [wakeTime, setWakeTime] = useState('');
   // const [challengeId, setChallengeId] = useState('');
@@ -77,6 +78,17 @@ const Settings = () => {
       alert(changeAffirmationError);
     }
   };
+
+  const ON_CLICK_HANDLERS = [
+    () => {
+      console.log('not host');
+      navigate('/giveUpChallenge');
+    },
+    () => {
+      console.log('host');
+      navigate('/editChallenge');
+    },
+  ];
 
   //////////////////////////////////////////
 
@@ -126,6 +138,12 @@ const Settings = () => {
       setAffirmation(user.myData.affirmation);
     }
   }, [user.myData.affirmation]);
+
+  useEffect(() => {
+    if (challengeData && challengeData.hostId) {
+      setIsHost(challengeData.hostId === userId);
+    }
+  }, [challengeData, userId]);
 
   return (
     <>
@@ -191,6 +209,9 @@ const Settings = () => {
         </> */}
 
         {/* <LongBtn btnName="연습 게임 진행하기" onClickHandler={handlePractice} /> */}
+        <ChallengeEditWrapper onClick={ON_CLICK_HANDLERS[+isHost]}>
+          {isHost ? <Text>챌린지 수정</Text> : <Text>챌린지 포기</Text>}
+        </ChallengeEditWrapper>
 
         <ChangePasswordWrapper onClick={() => navigate('/changePassword')}>
           <Text>비밀번호 변경</Text>
@@ -282,6 +303,10 @@ const LogoutWrapper = styled.div`
 `;
 
 const ChangePasswordWrapper = styled(LogoutWrapper)`
+  border: 2px solid ${({ theme }) => theme.colors.primary.emerald};
+`;
+
+const ChallengeEditWrapper = styled(LogoutWrapper)`
   border: 2px solid ${({ theme }) => theme.colors.primary.emerald};
 `;
 
